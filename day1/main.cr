4c9b3e642fe31ad203afc777290c047226562a38
Soma Yamamoto <leoreo2247@gmail.com>
2020-12-02 16:40:38 +0900

    day1 fix

--- day1/main.cr
+++ day1/main.cr
@@ -1,4 +1,5 @@
+require "benchmark"
 # Part 1
-p (File.read_lines("input.txt").map(&.to_i).each_permutation(2).find(&.sum.== 2020) || raise "not found").product
+p (File.read_lines("input.txt").map(&.to_i).each_combination(2).find(&.sum.== 2020) || raise "not found").product
 # Part2
-p (File.read_lines("input.txt").map(&.to_i).each_permutation(3).find(&.sum.== 2020) || raise "not found").product
+p (File.read_lines("input.txt").map(&.to_i).each_combination(3).find(&.sum.== 2020) || raise "not found").product
