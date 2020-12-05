6034e043e1336fec22e4bebd19c53028cd45abea
Soma Yamamoto <leoreo2247@gmail.com>
2020-12-06 00:09:05 +0900

    day2 fix

--- day2/main.cr
+++ day2/main.cr
@@ -1,5 +1,5 @@
 INPUT = File.read_lines("input.txt")
 # Part 1
-p INPUT.map{|l| i=l.split(/-| /); (i[0].to_i..i[1].to_i)===i[3].count(i[2][0])}.count(true)
+p INPUT.count { |l| i = l.split(/-| /); (i[0].to_i..i[1].to_i) === i[3].count(i[2][0]) }
 # Part 2
-p INPUT.map{|l| i=l.split(/-| /); [i[3][i[0].to_i-1], i[3][i[1].to_i-1]].count(i[2][0])}.count(1)
+p INPUT.count { |l| i = l.split(/-| /); [i[3][i[0].to_i-1], i[3][i[1].to_i-1]].count(i[2][0]) == 1 }
