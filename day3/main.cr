63625a0993477ed588142a3b6eff88342f33e773
Soma Yamamoto <leoreo2247@gmail.com>
2020-12-06 00:16:02 +0900

    day3 fix

--- day3/main.cr
+++ day3/main.cr
@@ -3,4 +3,4 @@ SLOPES = [{1,1}, {3,1}, {5,1}, {7,1}, {1,2}]
 # Part 1
 p INPUT.map_with_index { |l, y| l[(SLOPES[1][0]*y) % l.size]}.count('#')
 # Part 2
-p SLOPES.map { |s| x = 0; (0...INPUT.size).step(s[1]).map { |y| l = INPUT[y]; x += s[0] if y > 0; l[x % l.size] }.count('#') }.product
+p SLOPES.map { |s| x = 0; (0...INPUT.size).step(s[1]).count { |y| l = INPUT[y]; x += s[0] if y > 0; l[x % l.size] == '#' } }.product
